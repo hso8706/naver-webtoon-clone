fb3423c81c44848286f33a9a6efac60c6bd4ddd4
Ha Jung Ho <ggk14@naver.com>
2023-06-18 23:00:19 +0900

    first commit; 23.06.18

---
+++ server/src/main/java/com/example/naverwebtoonclone/NaverWebtoonCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.naverwebtoonclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverWebtoonCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaverWebtoonCloneApplication.class, args);
+    }
+
+}
