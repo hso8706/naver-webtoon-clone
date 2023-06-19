81ea7e28929c5b198a01826fb1251ebaa33f4ae5
Ha Jung Ho <ggk14@naver.com>
2023-06-19 23:38:11 +0900

    Ad Entity; 23.06.19

--- server/src/main/java/com/example/naverwebtoonclone/advertisement/entity/Ad.java
+++ server/src/main/java/com/example/naverwebtoonclone/advertisement/entity/Ad.java
@@ -1,5 +1,26 @@
 package com.example.naverwebtoonclone.advertisement.entity;
 
-public class Ad {
+import com.example.naverwebtoonclone.utils.audit.Auditable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Ad extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String contents;
 }
