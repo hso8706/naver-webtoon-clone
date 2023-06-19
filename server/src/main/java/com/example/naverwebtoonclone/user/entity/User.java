3f5ea112c86e5cecd1e9134f0a5f21a1fab98d52
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-06-19 23:39:39 +0900

    [Entity] Ad Entity, User Entity_23.06.19 (#3)
    * Ad Entity; 23.06.19
    
    * User Entity; 23.06.19

--- server/src/main/java/com/example/naverwebtoonclone/user/entity/User.java
+++ server/src/main/java/com/example/naverwebtoonclone/user/entity/User.java
@@ -1,5 +1,52 @@
 package com.example.naverwebtoonclone.user.entity;
 
-public class User {
+import com.example.naverwebtoonclone.utils.audit.Auditable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User extends Auditable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    @Enumerated(value = EnumType.STRING)
+    @Column
+    private UserStatus userStatus;
+
+
+    public enum UserStatus {
+        USER_ACTIVE("활동중"),
+        USER_SLEEP("휴면 상태"),
+        USER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        UserStatus(String status) {
+            this.status = status;
+        }
+    }
 
 }
