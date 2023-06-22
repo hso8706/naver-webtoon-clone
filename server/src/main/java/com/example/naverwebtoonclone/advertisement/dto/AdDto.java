1ac8aef6cca40ac645ddc2a3981419d0c008c27b
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-06-22 20:30:15 +0900

    Ad Basement; 23.06.22 (#6)

--- server/src/main/java/com/example/naverwebtoonclone/advertisement/dto/AdDto.java
+++ server/src/main/java/com/example/naverwebtoonclone/advertisement/dto/AdDto.java
@@ -1,5 +1,41 @@
 package com.example.naverwebtoonclone.advertisement.dto;
 
+import com.example.naverwebtoonclone.user.entity.User;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 public class AdDto {
 
+    @AllArgsConstructor
+    @Getter
+    public static class Post{
+
+        private String contents;
+        private String links;
+
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Patch{
+
+        private Long id;
+        private User etpId;
+        private String contents;
+        private String links;
+
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Response{
+
+        private Long id;
+        private String contents;
+        private String links;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+
+    }
 }
