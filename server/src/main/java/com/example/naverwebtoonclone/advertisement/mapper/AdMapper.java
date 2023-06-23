be00e363a3862674e9cc56edbea1daf15ca84677
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-06-23 13:38:11 +0900

    Revert "Ad Basement; 23.06.22 (#6)"
    This reverts commit 1ac8aef6cca40ac645ddc2a3981419d0c008c27b.

--- server/src/main/java/com/example/naverwebtoonclone/advertisement/mapper/AdMapper.java
+++ server/src/main/java/com/example/naverwebtoonclone/advertisement/mapper/AdMapper.java
@@ -1,17 +1,5 @@
 package com.example.naverwebtoonclone.advertisement.mapper;
 
-import com.example.naverwebtoonclone.advertisement.dto.AdDto;
-import com.example.naverwebtoonclone.advertisement.dto.AdDto.Response;
-import com.example.naverwebtoonclone.advertisement.entity.Ad;
-import java.util.List;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
 public interface AdMapper {
-
-    Ad postDtoToEntity (AdDto.Post postRequest);
-    Ad patchDtoToEntity (AdDto.Patch patchRequest);
-    AdDto.Response entityToResponse (Ad ad);
-    List<AdDto.Response> adListToResponseList (List<Ad> adList);
 
 }
