89771d15bd615d72b539bc40bbe5a2c5b78bd384
Jungho Ha <103169947+hso8706@users.noreply.github.com>
2023-06-23 13:38:22 +0900

    Revert "Ad Basement; 23.06.22 (#6)" (#9)
    This reverts commit 1ac8aef6cca40ac645ddc2a3981419d0c008c27b.

--- server/src/main/java/com/example/naverwebtoonclone/advertisement/repository/AdRepository.java
+++ server/src/main/java/com/example/naverwebtoonclone/advertisement/repository/AdRepository.java
@@ -1,8 +1,5 @@
 package com.example.naverwebtoonclone.advertisement.repository;
 
-import com.example.naverwebtoonclone.advertisement.entity.Ad;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdRepository extends JpaRepository<Ad, Long> {
+public interface AdRepository {
 
 }
